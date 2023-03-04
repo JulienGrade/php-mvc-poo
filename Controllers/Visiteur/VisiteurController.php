ee028a9582b2e26f1a331b4d7209be321479a591
JulienGrade <gradejulien@gmail.com>
2023-03-04 14:10:36 +0100

    3- Création bdd et ajout du premier modèle visiteur

--- Controllers/Visiteur/VisiteurController.php
+++ Controllers/Visiteur/VisiteurController.php
@@ -1,16 +1,26 @@
 <?php
 require_once('./Controllers/MainController.controller.php');
+require_once('./Models/Visiteur/Visiteur.model.php');
 class VisiteurController extends MainController
 {
+
+    private $visiteurManager;
+
+    public function __construct()
+    {
+        $this->visiteurManager = new VisiteurManager();
+    }
+
     //Propriété "page_css" : tableau permettant d'ajouter des fichiers CSS spécifiques
     //Propriété "page_javascript" : tableau permettant d'ajouter des fichiers JavaScript spécifiques
     public function accueil(): void
     {
         // Toolbox::ajouterMessageAlerte("test", Toolbox::COULEUR_VERTE);
-
+        $utilisateurs = $this->visiteurManager->getUtilisateurs();
         $data_page = [
             "page_description" => "Description de la page d'accueil",
             "page_title" => "Titre de la page d'accueil",
+            "utilisateurs" => $utilisateurs,
             "view" => "views/Visiteur/accueil.view.php",
             "template" => "views/partials/template.php"
         ];
